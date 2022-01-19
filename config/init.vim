69e8517d8944c2d62acdfda356b906f696b60845
lixin <bhlzlx@gmail.com>
2022-01-20 00:20:02 +0800

    add nvim-tree plugin

--- config/init.vim
+++ config/init.vim
@@ -1,3 +1,5 @@
 lua require('settings')
 lua require('plugins')
 lua require('keybindings')
+
+lua require('plugins_config/nvim_tree')
--- config/lua/plugins.lua
+++ config/lua/plugins.lua
@@ -1,6 +1,11 @@
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function()
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
-end)
+return require('packer').startup(
+    function()
+        -- Packer can manage itself
+        use 'wbthomason/packer.nvim'
+        use { 'kyazdani42/nvim-tree.lua',
+            requires = 'kyazdani42/nvim-web-devicons'
+        }
+    end
+)
